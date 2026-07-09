6710d1174ecde4d6007302d55184938c547fcd64
Claudio Luft <168219275+luftfloripa@users.noreply.github.com>
2026-07-09 17:57:52 -0300

    Delete PokerTracker directory

--- PokerTracker/src/main/java/br/com/pokertracker/repository/BancaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.pokertracker.repository;
-
-import br.com.pokertracker.model.Banca;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface BancaRepository extends JpaRepository<Banca, Long> {
-    List<Banca> findByUsuarioId(Long usuarioId);
-}
